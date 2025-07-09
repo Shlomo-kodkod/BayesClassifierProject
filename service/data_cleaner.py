397ca6d1810714c02d04ae502e2df24bef9df71a
kodcode <quarts.91.elders@icloud.com>
2025-07-09 15:49:18 +0300

    add clean data

--- service/data_cleaner.py
+++ service/data_cleaner.py
@@ -5,15 +5,15 @@ class DataCleaner:
         self.__index = int(len(self.__all_data) * 0.7)
         self.__all_data = data
 
-    @staticmethod
-    def clean_data():
-        pass
+    def clean_data(self):
+        for col in self.__all_data.columns:
+            self.__all_data[col].fillna(self.__all_data[col].value_counts().idxmax())
 
     def get_train_data(self):
         if self.__index:
             return self.__all_data[:self.__index]
             
-
     def get_test_data(self):
         if self.__index:
             return self.__all_data[:self.__index]
+        
--- service/data_loader.py
+++ service/data_loader.py
@@ -3,14 +3,6 @@ from abc import ABC, abstractmethod
 class DataLoader:
 
     @abstractmethod
-    def data_loader(self):
-        pass
-
-    @abstractmethod
-    def get_training_data(self):
-        pass
-
-    @abstractmethod
-    def get_test_data(self):
+    def data_loader():
         pass
 
